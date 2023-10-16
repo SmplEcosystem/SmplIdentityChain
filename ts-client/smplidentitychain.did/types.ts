911e623b65fa9249e6f9d6cde56b99ac230510e5
Jacob Boswell <jacob@s56.net>
2023-10-16 14:43:17 -0600

    vm type

---
+++ ts-client/smplidentitychain.did/types.ts
@@ -0,0 +1,9 @@
+import { Params } from "./types/smplidentitychain/did/params"
+import { VerificationMethod } from "./types/smplidentitychain/did/verification_method"
+
+
+export {     
+    Params,
+    VerificationMethod,
+    
+ }
---
+++ ts-client/smplidentitychain.vc/types.ts
@@ -0,0 +1,7 @@
+import { Params } from "./types/smplidentitychain/vc/params"
+
+
+export {     
+    Params,
+    
+ }
