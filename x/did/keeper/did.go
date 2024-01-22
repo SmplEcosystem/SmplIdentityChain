a62d86dfdb9acc3999e2b4b60ee62ce539bf090d
Noah Boswell <noah@s56.net>
2024-01-22 11:53:28 -0700

    Set up some stuff

--- x/did/keeper/did.go
+++ x/did/keeper/did.go
@@ -7,10 +7,10 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-func (k Keeper) SetDid(ctx sdk.Context, doc types.DIDDocument) {
+func (k Keeper) SetDid(ctx sdk.Context, msg types.MsgUpsertDid) {
 	storeAdapter := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
 	store := prefix.NewStore(storeAdapter, types.DIDKeyPrefix)
-	key := []byte(doc.Id)
-	bz := k.cdc.MustMarshalLengthPrefixed(&doc)
+	key := []byte(msg.DidDocument.Id)
+	bz := k.cdc.MustMarshalLengthPrefixed(&msg)
 	store.Set(key, bz)
 }
