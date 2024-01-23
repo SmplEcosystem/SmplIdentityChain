e887221fe152d6af6ced75f876948920bda529ed
Noah Boswell <noah@s56.net>
2024-01-23 12:23:42 -0700

    help with message

--- x/did/keeper/did.go
+++ x/did/keeper/did.go
@@ -7,10 +7,23 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-func (k Keeper) SetDid(ctx sdk.Context, msg types.MsgUpsertDid) {
+func (k Keeper) SetDid(ctx sdk.Context, msg *types.MsgUpsertDid) {
 	storeAdapter := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
 	store := prefix.NewStore(storeAdapter, types.DIDKeyPrefix)
 	key := []byte(msg.DidDocument.Id)
-	bz := k.cdc.MustMarshalLengthPrefixed(&msg)
+	bz := k.cdc.MustMarshalLengthPrefixed(msg)
 	store.Set(key, bz)
+}
+func (k Keeper) GetDIDDocument(ctx sdk.Context, did string) types.QueryResolveDidRequestResponse {
+	storeAdapter := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
+	store := prefix.NewStore(storeAdapter, types.DIDKeyPrefix)
+	key := []byte(did)
+	bz := store.Get(key)
+	if bz == nil {
+		return types.QueryResolveDidRequestResponse{}
+	}
+
+	var doc types.QueryResolveDidRequestResponse
+	k.cdc.MustUnmarshalLengthPrefixed(bz, &doc)
+	return doc
 }
--- x/did/keeper/msg_server_upsert_did.go
+++ x/did/keeper/msg_server_upsert_did.go
@@ -2,15 +2,13 @@ package keeper
 
 import (
 	"context"
-
 	"github.com/SmplEcosystem/SmplIdentityChain/x/did/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) UpsertDid(goCtx context.Context, msg *types.MsgUpsertDid) (*types.MsgUpsertDidResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	// TODO: Handling the message
+	k.SetDid(ctx, msg)
 	_ = ctx
 
 	return &types.MsgUpsertDidResponse{}, nil
