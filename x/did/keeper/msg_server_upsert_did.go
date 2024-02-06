75ce03063005438b8e0c0b351c9b34e0c07a00c1
Noah Boswell <noah@s56.net>
2024-02-06 15:23:37 -0700

    set up meta data managment

--- x/did/keeper/msg_server_upsert_did.go
+++ x/did/keeper/msg_server_upsert_did.go
@@ -7,19 +7,19 @@ import (
 )
 
 func (k msgServer) UpsertDid(goCtx context.Context, msg *types.MsgUpsertDid) (*types.MsgUpsertDidResponse, error) {
-	//var seq = uint64(0)
+	var seq = uint64(0)
 	ctx := sdk.UnwrapSDKContext(goCtx)
-	//DocumentToChange := k.GetDIDDocument(ctx, msg.DidDocument.Id)
-	//if DocumentToChange == nil {
-	//	seq = 0
-	//} else {
-	//	seq = DocumentToChange.Sequence
-	//}
+	DocumentToChange := k.GetDIDDocument(ctx, msg.DidDocument.Id)
+	if DocumentToChange == nil {
+		seq = 0
+	} else {
+		seq = DocumentToChange.Sequence
+	}
 
-	//_, err := k.VerifyDidOwnership(msg.DidDocument, seq, msg.Signature, msg.VerificationMethodId)
-	//if err != nil {
-	//	return nil, err
-	//}
+	_, err := k.VerifyDidOwnership(msg.DidDocument, seq, msg.Signature, msg.VerificationMethodId)
+	if err != nil {
+		return nil, err
+	}
 	k.SetDid(ctx, msg)
 	_ = ctx
 
